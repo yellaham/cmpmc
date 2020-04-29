2d9967db0f7a880d5ddbe2bc05c23bac139cf5b3
Yousef El-Laham <yousef.ellaham@stonybrook.edu>
2020-04-29 17:19:23 -0400

    updated header of cmpmc function

--- samplers.py
+++ samplers.py
@@ -7,29 +7,29 @@ from tqdm import tqdm
 # Define a class for adaptive importance samplers with adaptive mixtures
 class MPMCSampler:
     def __init__(self, x, log_w, rho, mu, sigma, z_est, mu_est):
-        self.particles = x
-        self.log_weights = log_w
-        self.mix_weights = rho
-        self.means = mu
-        self.covariances = sigma
-        self.evidence = z_est
-        self.target_mean = mu_est
+        self.particles = x          # samples
+        self.log_weights = log_w    # log weights
+        self.mix_weights = rho      # weight of each mixture component
+        self.means = mu             # mean of each mixture component
+        self.covariances = sigma    # covariance matrix of each mixture component
+        self.evidence = z_est       # unbiased estimate of the normalization constant
+        self.target_mean = mu_est   # self-normalized estimate of the target mean (MMSE)
 
 
 # Define a class for adaptive importance samplers which adapt the locations of a population of proposals
 class APISSampler:
     def __init__(self, x, log_w, mu, z_est, mu_est):
-        self.particles = x
-        self.log_weights = log_w
-        self.means = mu
-        self.evidence = z_est
-        self.target_mean = mu_est
+        self.particles = x          # samples
+        self.log_weights = log_w    # log weights
+        self.means = mu             # locations of the population of proposals
+        self.evidence = z_est       # unbiased estimate of the normalization constant
+        self.target_mean = mu_est   # self-normalized estimate of the target mean (MMSE)
 
 
 # Define a class for Markov Chain Monte Carlo samplers
 class MCMCSampler:
     def __init__(self, x):
-        self.samples = x
+        self.samples = x            # samples from the target distribution (MCMC chain)
 
 
 # Implementation of the MH method
@@ -41,10 +41,10 @@ def metropolis_hastings(log_target, initial_state, var_x=1, T=1000, burn_in=0, t
     :param var_x: Variance of the Markov transition kernel
     :param T: Length of generated Markov chain
     :param burn_in: Number of samples to discard from the beginning of the chain
-    :param thinning rate: Thinning of the chain to reduce autocorrelation between samples
+    :param thinning_rate: Thinning of the chain to reduce autocorrelation between samples
     :return particles
     """
-    # Determine dimension from intial state
+    # Determine dimension from initial state
     d = np.shape(initial_state)[0]
 
     # Initialize storage of particles and log weights
@@ -87,25 +87,25 @@ def cmpmc(log_target, d, D=10, M=50, I=200, K=5, var_prop=1, bounds=(-10, 10), a
     Runs the proposed controlled mixture population Monte Carlo algorithm
     :param log_target: Logarithm of the target distribution
     :param d: Dimension of the sampling space
-    :param D: Number of proposals
-    :param M: Number of samples to draw
+    :param D: Number of mixture components
+    :param M: Number of samples to draw per iteration
     :param I: Number of iterations
     :param K: Number of MH steps at each iteration for each Markov chain
-    :param var_prop: Variance of each proposal distribution
-    :param bounds: Prior to generate location parameters over [bounds]**d hypercube
+    :param var_prop: Initial variance of each proposal distribution
+    :param bounds: Prior to generate mixture means over [bounds]**d hypercube
     :param alpha: Renyi divergence parameter (must be greater than 1)
     :param eta_rho0: Initial learning rate for mixture weights
-    :param eta_mu0: Initial learning rate for mixand mean
-    :param eta_prec0: Initial learning rate for mixand precision matrix
+    :param eta_mu0: Initial learning rate for mixture component means
+    :param eta_prec0: Initial learning rate for mixture component precision matrix
     :param g_rho_max: Maximum norm of mixture weight gradient
-    :param g_mu_max: Maximum norm of mixand mean gradient
-    :param g_prec_max: Maximum norm of mixand precision matrix gradient
+    :param g_mu_max: Maximum norm of mixture mean gradient
+    :param g_prec_max: Maximum norm of mixture precision matrix gradient
     :param Kthin: Thinning parameter for the MH algorithm
-    :param var_mcmc: Pertubration variance of the MCMC state
-    :return particles, weights, and estimate of normalizing constant
+    :param var_mcmc: Perturbation variance of the MCMC state
+    :return MPMCSampler object
     """
     # Initialize the weights of the mixture proposal
-    rho = np.ones(D) / D
+    rho = np.ones(D)/D
 
     # Initialize the means of the mixture proposal
     mu = np.random.uniform(bounds[0], bounds[1], (D, d))
@@ -115,11 +115,11 @@ def cmpmc(log_target, d, D=10, M=50, I=200, K=5, var_prop=1, bounds=(-10, 10), a
     prec = np.tile((1/var_prop) * np.eye(d), (D, 1, 1))
 
     # Initialize storage of particles and log weights
-    particles = np.zeros((M * I, d))
-    log_weights = np.ones(M * I) * (-np.inf)
-    mix_weights = np.zeros((I + 1, D))
-    means = np.zeros((D * (I + 1), d))
-    covariances = np.tile(np.zeros((d, d)), (D * (I + 1), 1, 1))
+    particles = np.zeros((M*I, d))
+    log_weights = np.ones(M*I)*(-np.inf)
+    mix_weights = np.zeros((I+1, D))
+    means = np.zeros((D*(I+1), d))
+    covariances = np.tile(np.zeros((d, d)), (D*(I+1), 1, 1))
 
     # Initialize storage of evidence and target mean estimates
     evidence = np.zeros(I)
@@ -144,14 +144,14 @@ def cmpmc(log_target, d, D=10, M=50, I=200, K=5, var_prop=1, bounds=(-10, 10), a
     # Loop for the algorithm
     for i in tqdm(range(I)):
         # Update start counter
-        stop = start + M
-        stopd = startd + D
+        stop = start+M
+        stopd = startd+D
 
         # Generate particles
         idx = np.random.choice(D, M, replace=True, p=rho)
         children = np.random.multivariate_normal(np.zeros(d), np.eye(d), M)
         for j in range(D):
-            children[idx == j] = mu[j] + np.matmul(children[idx == j], np.linalg.cholesky(sig[j]).T)
+            children[idx==j] = mu[j]+np.matmul(children[idx == j], np.linalg.cholesky(sig[j]).T)
         particles[start:stop] = children
 
         # Compute log proposal
@@ -161,15 +161,12 @@ def cmpmc(log_target, d, D=10, M=50, I=200, K=5, var_prop=1, bounds=(-10, 10), a
         # Find the maximum log weight
         max_log_weight = np.max(log_prop_j)
         # Determine the equivalent log proposal as
-        log_prop = max_log_weight + np.log(np.average(np.exp(log_prop_j-max_log_weight), weights=rho, axis=1))
+        log_prop = max_log_weight+np.log(np.average(np.exp(log_prop_j-max_log_weight), weights=rho, axis=1))
 
         # Compute log weights and store
         log_target_eval = log_target(children)
-        log_w = log_target_eval - log_prop
+        log_w = log_target_eval-log_prop
         log_weights[start:stop] = log_w
-
-        # Convert log weights to standard weights using LSE and normalize
-        w = np.exp(log_w - np.max(log_w))
 
         # Compute estimate of evidence
         max_log_weight = np.max(log_weights[0:stop])
@@ -195,7 +192,7 @@ def cmpmc(log_target, d, D=10, M=50, I=200, K=5, var_prop=1, bounds=(-10, 10), a
             prop_z_k = np.zeros((np.shape(z_d)[0], D))
             for jk in range(D):
                 prop_z_k[:, jk] = mvn.pdf(z_d, mean=mu_temp[jk], cov=sig_temp[jk], allow_singular=True)
-                prop_z += rho_temp[jk] * prop_z_k[:, jk]
+                prop_z += rho_temp[jk]*prop_z_k[:, jk]
             log_prop_z = np.log(prop_z)
             log_target_z = np.asarray([log_target(z_d)]).T
 
@@ -205,11 +202,11 @@ def cmpmc(log_target, d, D=10, M=50, I=200, K=5, var_prop=1, bounds=(-10, 10), a
             g_prec = np.zeros((d, d))           # initialize gradient of precision matrix
             # Use a loop to make sure you are doing it correctly
             for k in range(np.shape(z_d)[0]):
-                factor = np.exp((alpha-1)*(log_target_z[k] - log_prop_z[k]))*(prop_z_k[k, j]/prop_z[k])
+                factor = np.exp((alpha-1)*(log_target_z[k]-log_prop_z[k]))*(prop_z_k[k, j]/prop_z[k])
                 g_rho += factor
                 temp_var = np.asmatrix((z_d[k]-mu[j])).T
                 g_mu += factor*np.asarray(np.matmul(prec[j], temp_var)).squeeze()
-                g_prec += (1/2)*(sig_temp[j] - temp_var*temp_var.T)
+                g_prec += (1/2)*(sig_temp[j]-temp_var*temp_var.T)
             g_rho *= (1-alpha)/np.shape(z_d)[0]
             g_mu *= (rho_temp[j]*(1-alpha))/np.shape(z_d)[0]
             g_prec *= (rho_temp[j]*(1-alpha))/np.shape(z_d)[0]
@@ -224,31 +221,31 @@ def cmpmc(log_target, d, D=10, M=50, I=200, K=5, var_prop=1, bounds=(-10, 10), a
 
             # PART 5: Taking the stochastic gradient step
             # Compute square of the gradient
-            drho_sq = g_rho ** 2
-            dmu_sq = g_mu ** 2
-            dprec_sq = g_prec ** 2
+            drho_sq = g_rho**2
+            dmu_sq = g_mu**2
+            dprec_sq = g_prec**2
             # Update elementwise learning rate parameters
-            vrho[j] = 0.9 * vrho[j] + 0.1 * drho_sq
-            vmu[j] = 0.9 * vmu[j] + 0.1 * dmu_sq
-            vprec[j] = 0.9 * vprec[j] + 0.1 * dprec_sq
+            vrho[j] = 0.9*vrho[j] + 0.1*drho_sq
+            vmu[j] = 0.9*vmu[j] + 0.1*dmu_sq
+            vprec[j] = 0.9*vprec[j] + 0.1*dprec_sq
             # Compute the learning rates
-            eta_rho = eta_rho0 * (np.sqrt(vrho[j]) + 1e-3) ** (-1)
-            eta_mu = eta_mu0 * (np.sqrt(vmu[j]) + 1e-3) ** (-1)
-            eta_prec = eta_prec0 * (np.sqrt(vprec[j]) + 1e-3) ** (-1)
+            eta_rho = eta_rho0*(np.sqrt(vrho[j])+1e-3)**(-1)
+            eta_mu = eta_mu0*(np.sqrt(vmu[j])+1e-3)**(-1)
+            eta_prec = eta_prec0*(np.sqrt(vprec[j])+1e-3)**(-1)
             # Make stochastic gradient updates
             if eta_rho0 > 0:
-                rho[j] = rho[j] - eta_rho * g_rho
+                rho[j] = rho[j]-eta_rho*g_rho
             if eta_mu0 > 0:
-                mu[j] = mu[j] - eta_mu * g_mu
+                mu[j] = mu[j]-eta_mu*g_mu
                 chain[j] = mu[j]
             if eta_prec0 > 0:
                 # Update and project the precision matrix onto the set of PSD matrices
-                prec[j] = prec[j] - eta_prec * g_prec
+                prec[j] = prec[j]-eta_prec*g_prec
                 # Obtain the corresponding covariance matrix
                 sig[j] = np.linalg.inv(prec[j])
 
-        # Add small number to weights and normalize
-        rho = other_funcs.projection_simplex_sort(rho + 1e-3)
+        # Add small number to weights and project onto the simplex
+        rho = other_funcs.projection_simplex_sort(rho+1e-3)
 
         # Store parameters
         mix_weights[i + 1] = rho
